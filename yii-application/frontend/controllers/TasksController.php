598df714f17dae380258186b574625a43f51debe
surizmen <karach.kirill@bk.ru>
2019-12-02 15:38:36 +0700

    authorization

--- yii-application/frontend/controllers/TasksController.php
+++ yii-application/frontend/controllers/TasksController.php
@@ -1,11 +1,23 @@
 <?php
 namespace frontend\controllers;
-
+use app\models\Tasks;
+use yii\data\ActiveDataProvider;
+use yii\db\Query;
 use yii\rest\ActiveController;
+use Yii;
 
 class TasksController extends BaseApiController
 {
-    public $modelClass = 'app\models\Tasks';
+    public $modelClass = Tasks::class;
+
+    public function actions()
+    {
+        $actions = parent::actions();
+
+        //Переопределение дата провайдера
+        $actions['index']['prepareDataProvider']=[$this,'get_tasks'];
+        return $actions;
+    }
 
     public function behaviors() {
         $behaviors = parent::behaviors();
@@ -21,10 +33,22 @@ class TasksController extends BaseApiController
         ];
         $behaviors['authenticator'] = [
             'class' => \yii\filters\auth\HttpBearerAuth::className(),
+            //  действия "delete" и "create" и "index" только для авторизированных пользователей
             'only'=>['update','create','delete']
         ];
         return $behaviors;
+
     }
+    //Переопределение дата провайдера где выводи обЪявления только со статусом АКтивные
+    public function get_tasks(){
+        return Yii::createObject(
+            [
+                'class' => ActiveDataProvider::class,
+                'query' => Tasks::find()->where(['tasks_status_number' => 1])
+            ]
+        );
+    }
+
 
 }
 
