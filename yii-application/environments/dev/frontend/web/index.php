43de9fd9e1a8ed8f750c87c2b3e7a27edf48c64a
surizmen <karach.kirill@bk.ru>
2019-12-06 21:36:30 +0700

    Tasks_search image_upload,

--- yii-application/environments/dev/frontend/web/index.php
+++ yii-application/environments/dev/frontend/web/index.php
@@ -2,6 +2,7 @@
 defined('YII_DEBUG') or define('YII_DEBUG', true);
 defined('YII_ENV') or define('YII_ENV', 'dev');
 
+
 require __DIR__ . '/../../vendor/autoload.php';
 require __DIR__ . '/../../vendor/yiisoft/yii2/Yii.php';
 require __DIR__ . '/../../common/config/bootstrap.php';
@@ -10,6 +11,7 @@ require __DIR__ . '/../config/bootstrap.php';
 $config = yii\helpers\ArrayHelper::merge(
     require __DIR__ . '/../../common/config/main.php',
     require __DIR__ . '/../../common/config/main-local.php',
+    require __DIR__ . '/../../common/config/bd-connection.php',
     require __DIR__ . '/../config/main.php',
     require __DIR__ . '/../config/main-local.php'
 );
